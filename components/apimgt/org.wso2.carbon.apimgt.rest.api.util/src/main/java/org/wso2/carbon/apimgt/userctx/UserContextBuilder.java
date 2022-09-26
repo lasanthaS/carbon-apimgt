c137320d978a738b40f108d1e0c48b50d735d835
lasanthaS <lasn1987@gmail.com>
2022-09-26 09:17:25 +0530

    Add user context and proxies

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.util/src/main/java/org/wso2/carbon/apimgt/userctx/UserContextBuilder.java
+++
@@ -1,9 +0,0 @@
-package org.wso2.carbon.apimgt.userctx;
-
-import java.util.Map;
-
-public interface UserContextBuilder {
-    Map<String, Object> getProperties();
-
-    String getClaim(String claimUri);
-}
--- components/apimgt/org.wso2.carbon.apimgt.rest.api.util/src/main/java/org/wso2/carbon/apimgt/userctx/UserContextConstants.java
+++
@@ -1,8 +0,0 @@
-package org.wso2.carbon.apimgt.userctx;
-
-public class UserContextConstants {
-    public static final String ATTRIB_USERNAME = "username";
-    public static final String ATTRIB_ORGANIZATION = "organization";
-    public static final String ATTRIB_ROLES = "roles";
-    public static final String DOT = ".";
-}
